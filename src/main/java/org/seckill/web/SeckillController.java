66b8d0b4e87fea7d4eeb730b22d2c0a2d1383ec7
linqiudi <qiu_di_lin@163.com>
2018-07-30 23:41:34 +0800

    web层编写

---
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -0,0 +1,96 @@
+package org.seckill.web;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.enums.SeckillStateEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model){
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model){
+        if(seckillId == null){
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if(seckill == null){
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+    //返回json数据
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST, produces = {"appication/json;charset-UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId){
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<>(true, exposer);
+        }catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result = new SeckillResult<>(false, e.getMessage());
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+                    method = RequestMethod.POST,
+                    produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId,
+                                                   @PathVariable("md5") String md5,
+                                                   @CookieValue(value = "killphone", required = false)Long phone){
+       if(phone == null){
+           return new SeckillResult<SeckillExecution>(false, "未注册");
+       }
+        try {
+            SeckillExecution execution = seckillService.executeSeckill(seckillId, phone, md5);
+            return new SeckillResult<>(true, execution);
+        } catch (RepeatKillException e){
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStateEnum.REPEATE_KILL);
+            return new SeckillResult<>(false, execution);
+        } catch (SeckillCloseException e){
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStateEnum.END);
+            return new SeckillResult<>(false, execution);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStateEnum.INNER_ERROR);
+            return new SeckillResult<>(false, execution);
+        }
+    }
+
+    @RequestMapping(value = "/time/now", method = RequestMethod.GET)
+    public SeckillResult<Long> time(){
+        Date now = new Date();
+        return new SeckillResult(true, now.getTime());
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/detail.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: li你
+  Date: 2018/7/30
+  Time: 22:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>秒杀详情页</title>
+    <%@include file="common/head.jsp"%>
+</head>
+<body>
+    <div class="container">
+        <div class="panel panel-default text-center">
+            <div class="panel-heading">
+               <h1>${seckill.name}</h1>
+            </div>
+        </div>
+        <div class="panel-body">
+
+        </div>
+    </div>
+</body>
+</html>
